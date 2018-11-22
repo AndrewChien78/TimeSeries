6b7cb042ced2617197e3c70aea800f429420f493
Andrew Chien <andrewkb.chien@gmail.com>
2018-11-22 16:42:17 +0800

    updated bookings.py Champagne.py Decompose.py airline.py bookings.csv

--- Champagne.py
+++ Champagne.py
@@ -6,6 +6,8 @@
 #
 from pandas import Series
 from matplotlib import pyplot
+from statsmodels.tsa.seasonal import seasonal_decompose
 series = Series.from_csv('champagne-sales.csv', header=0)
-series.plot()
+result = seasonal_decompose(series, model='additive',freq=1)
+result.plot()
 pyplot.show()
--- airline3.py
+++ airline3.py
@@ -1,10 +1,11 @@
 # This is the airline passengers example from Jason Brownlee of Machine Learning Mastery. This code provides 
 # an example of time series decomposition
 # https://machinelearningmastery.com/decompose-time-series-data-trend-seasonality/
-from pandas import Series
+# this file uses the read_csv function instead of Series function
+from pandas import read_csv
 from matplotlib import pyplot
 from statsmodels.tsa.seasonal import seasonal_decompose
-series = Series.from_csv('international-airline-passengers.csv', header=0)
+series = read_csv('international-airline-passengers.csv', header=0,parse_dates=[0],index_col=0,squeeze=True)
 result = seasonal_decompose(series, model='multiplicative')
 result.plot()
 pyplot.show()
